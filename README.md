c40cfec6823e560eca1388c7e0ace45263f3724b
kamlakant21 <kamlakantbag@gmail.com>
2023-07-06 22:19:17 +0530

    created-register-page

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # medical-app
+
 A capstone project
+
+To install Fontawesome
+
+https://fontawesome.com/v5/docs/web/use-with/react
+
+npm i --save @fortawesome/fontawesome-svg-core
+npm install --save @fortawesome/free-solid-svg-icons
+npm install --save @fortawesome/react-fontawesome
+
+For Regexr
+
+https://regexr.com/
+
+Install Axios
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
 import React from "react";
 import Ocr from "./components/Ocr";
-import Homepage from "./components/Homepage"
+import Homepage from "./components/Homepage";
 import Medicalreport from "./components/Medicalreport";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import Register from "./components/Register";
 
 const App = () => {
   return (
     <main>
-      <div className='main'>
-        <div className='gradient' />
+      <div className="main">
+        <div className="gradient" />
       </div>
-
-      <div className='app'>
-
-
+      <div className="app">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Homepage />}>
-            </Route>
-            <Route path="ocr" element={<Ocr />}>
-
-            </Route>
-            <Route path="medical" element={<Medicalreport />}>
-
-            </Route>
+            <Route path="homepage" element={<Homepage />}></Route>
+            <Route path="/" element={<Register />}></Route>
+            <Route path="ocr" element={<Ocr />}></Route>
+            <Route path="medical" element={<Medicalreport />}></Route>
           </Routes>
         </BrowserRouter>
-
       </div>
     </main>
   );
