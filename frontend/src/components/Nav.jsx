de43587d695e546359dbfe4d7edf0d75a077b00f
kamlakant21 <kamlakantbag@gmail.com>
2023-07-19 21:17:58 +0530

    ui-changes

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -6,9 +6,8 @@ import "./hero.css";
 const Nav = () => {
   return (
     <>
-      {/* <nav class="flex justify-center space-x-4 py-8"> */}
-      <nav className="flex justify-between items-center w-full mb-10 pt-3 w-full flex-row">
-        <Link to="/homepage" style={{ marginRight: "auto" }}>
+      <nav className="flex justify-between items-center mb-10 pt-3 w-full flex-row">
+        <Link to="/" style={{ marginRight: "auto" }}>
           <img
             src={logo}
             alt="sumz_logo"
@@ -18,27 +17,22 @@ const Nav = () => {
 
         <a
           href="/physicians"
-          class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+          className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
           Physicians
         </a>
         <a
-          href="/reports"
-          class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-          Reports
-        </a>
-        <a
           href="/ocr"
-          class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+          className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
           Ocr
         </a>
         <a
           href="/drugs"
-          class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+          className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
           Drugs
         </a>
         <a
           href="/profile"
-          class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+          className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
           Profile
         </a>
 
